3e16e51593b38ad296824e37f76c611c1d4bc950
Ahmed-Raza4 <hafizahmedraza12345@gmail.com>
2024-10-19 09:32:39 +0500

    add hide option in experience

--- Mileston1_2/script.ts
+++ Mileston1_2/script.ts
@@ -1,6 +1,16 @@
 const toggleSkillsButton = document.getElementById("toggleSkillsButton");
 const skillsSection = document.getElementById("skillsSection");
+const toggleExperienceButton = document.getElementById("toggleExperienceButton")
+const experienceSection = document.getElementById("experienceSection");
 
+
+toggleExperienceButton?.addEventListener("click", ()=>{
+  if (experienceSection?.style.display == "none"){
+    experienceSection.style.display = "block";
+  }else{
+    experienceSection.style.display = "none";
+  }
+})
 toggleSkillsButton?.addEventListener("click", () => {
   if (skillsSection?.style.display === "none") {
     skillsSection.style.display = "block";
